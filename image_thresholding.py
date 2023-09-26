02dd896321b9e99c564f1b9f9ad8b24a689c2828
madelinesimpson <91549090+madelinesimpson@users.noreply.github.com>
2023-09-26 10:55:44 -0400

    Update image_thresholding.py

--- image_thresholding.py
+++ image_thresholding.py
@@ -1,43 +1,74 @@
-import cv2
+import numpy
+from segment_anything import sam_model_registry, SamPredictor, SamAutomaticMaskGenerator
+import tensorflow as tf
+import torch
+from tensorflow import keras
 import matplotlib.pyplot as plt
 import numpy as np
+import cv2
 from tensorflow.keras.preprocessing import image
+import pickle
 
-#Insert your own local file directory for the image (testworm.jpg) here
+'''
+Sam Setup
+'''
+sam_checkpoint = "sam_vit_h_4b8939.pth"
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+model_type = "vit_h"
+sam = sam_model_registry[model_type](checkpoint=sam_checkpoint)
+sam.to(device=device)
+mask_generator_2 = SamAutomaticMaskGenerator(
+    model=sam,
+    points_per_side=32,
+    pred_iou_thresh=0.9,
+    stability_score_thresh=0.92,
+    crop_n_layers=1,
+    crop_n_points_downscale_factor=2,
+    min_mask_region_area=400,  # Requires open-cv to run post-processing
+)
+
+#Insert your specific path to the image here
 file = '/Users/madelinesimpson/PycharmProjects/HTR/testworm.jpg'
 
-img = image.load_img(file)
+'''
+Process the image to be black and white and remove as many unwanted blobs as possible
+'''
+def preprocess_image(file):
+    img = image.load_img(file)
 
-img_array = image.img_to_array(img, dtype="uint8")
-img_array = cv2.cvtColor(img_array, cv2.COLOR_BGR2GRAY)
+    #Make the image an array and convert to grayscale
+    img_array = image.img_to_array(img, dtype="uint8")
+    img_array = cv2.cvtColor(img_array, cv2.COLOR_BGR2GRAY)
 
-median_blur_image = cv2.medianBlur(img_array, 5)
+    #Get those worms to pop
+    median_blur_image = cv2.medianBlur(img_array, 5)
+    gaussian_thresh_image = cv2.adaptiveThreshold(median_blur_image,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
+                                      cv2.THRESH_BINARY,11,2)
+    gaussian_blur_image = cv2.GaussianBlur(gaussian_thresh_image,(5,5),0)
 
-gaussian_thresh_image = cv2.adaptiveThreshold(median_blur_image,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-                                  cv2.THRESH_BINARY,11,2)
+    ret, otsu_thresh_image = cv2.threshold(gaussian_blur_image,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    contours, hierarchy = cv2.findContours(otsu_thresh_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-gaussian_blur_image = cv2.GaussianBlur(gaussian_thresh_image,(5,5),0)
+    #Remove small blobs from the background
+    threshold_blobs_area = 500
+    for i in range(1, len(contours)):
+        index_level = int(hierarchy[0][i][1])
+        if (index_level<=i):
+            cnt = contours[i]
+            area = cv2.contourArea(cnt)
+            if (area<=threshold_blobs_area):
+                cv2.drawContours(otsu_thresh_image, [cnt], -1, 255, -1, 1)
 
-ret, otsu_thresh_image = cv2.threshold(gaussian_blur_image,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    #Convert image to RGB
+    rgb = cv2.cvtColor(otsu_thresh_image, cv2.COLOR_BGR2RGB)
+    return rgb
 
-contours, hierarchy = cv2.findContours(otsu_thresh_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-max_threshold_blobs_area = 1000
-min_threshold_blobs_area = 500
+'''
+Let SAM generate the masks
+'''
+processed_image = preprocess_image(file)
+masks = mask_generator_2.generate(processed_image)
 
-#Draw over contours that are too small
-for i in range(1, len(contours)):
-    index_level = int(hierarchy[0][i][1])
-    if (index_level<=i):
-        cnt = contours[i]
-        area = cv2.contourArea(cnt)
-        if (area<=min_threshold_blobs_area or area>=max_threshold_blobs_area):
-            cv2.drawContours(otsu_thresh_image, [cnt], -1, 255, -1, 1)
-
-rgb = cv2.cvtColor(otsu_thresh_image, cv2.COLOR_BGR2RGB)
-
-
-
-
-plt.figure(figsize=(10,10))
-plt.imshow(rgb)
-plt.show()
+with open('/Users/madelinesimpson/PycharmProjects/HTR/masks.pkl', 'wb') as fp:
+    pickle.dump(masks, fp)
+    print('masks saved successfully to file')
