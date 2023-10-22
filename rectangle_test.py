702904a38af8e83eb679b218998990d7a11c92e3
madelinesimpson <91549090+madelinesimpson@users.noreply.github.com>
2023-10-22 16:07:23 -0400

    Update rectangle_test.py

--- rectangle_test.py
+++ rectangle_test.py
@@ -7,23 +7,30 @@ import math
 from sympy.solvers import solve
 from sympy import Symbol
 
+
+#-----------------------------------CODE TAKEN FROM THIEN WORMS GITHUB-----------------------------------
+
+#Get the neighbors of a particular pixel in an image
 def neighbours(x,y,image):
 	img = image
 	x_1, y_1, x1, y1 = x-1, y-1, x+1, y+1
-	return [ img[x_1][y], img[x_1][y1], img[x][y1], img[x1][y1],	 # P2,P3,P4,P5
-				img[x1][y], img[x1][y_1], img[x][y_1], img[x_1][y_1] ]	# P6,P7,P8,P9
+	return [ img[x_1][y], img[x_1][y1], img[x][y1], img[x1][y1],
+				img[x1][y], img[x1][y_1], img[x][y_1], img[x_1][y_1] ]
 
+#The same thing? I took this portion of the code so not sure why they repeat function
 def get_neighbours(x,y,image):
 	img = image
 	x_1, y_1, x1, y1 = x-1, y-1, x+1, y+1
-	return [ img[x_1][y], img[x_1][y1], img[x][y1], img[x1][y1],	 # P2,P3,P4,P5
-				img[x1][y], img[x1][y_1], img[x][y_1], img[x_1][y_1] ]	# P6,P7,P8,P9
+	return [ img[x_1][y], img[x_1][y1], img[x][y1], img[x1][y1],
+				img[x1][y], img[x1][y_1], img[x][y_1], img[x_1][y_1] ]
 
+#Helper function for the skeleton algorithm
 def transitions(neighbour, white):
 	count = 0
-	n = neighbour + neighbour[0:1]	  # P2, P3, ... , P8, P9, P2
-	return sum( (n1, n2) == (0, 1) for n1, n2 in zip(n, n[1:]) )  # (P2,P3), (P3,P4), ... , (P8,P9), (P9,P2)
+	n = neighbour + neighbour[0:1]
+	return sum( (n1, n2) == (0, 1) for n1, n2 in zip(n, n[1:]) )
 
+#Algorithm to turn a worm into its skeleton
 def zhangSuen(image):
 	image = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
 	white = 1
@@ -69,6 +76,7 @@ def zhangSuen(image):
 	skeleton = skeleton * 255
 	return skeleton
 
+#Find the endpoints of the skeleton of the worm
 def findEndpoints(skeleton):
 	(rows,cols) = np.nonzero(skeleton)
 	skel_coords = []
@@ -79,18 +87,17 @@ def findEndpoints(skeleton):
 			skel_coords.append((r,c))
 	return skel_coords
 
+#Counts how many white pixels surround a particular pixel
 def countNeighbouringPixels(skeleton, x, y):
     neighbours = get_neighbours(x,y,skeleton)
     return sum(neighbours) / 255
 
-def countSkeletonPixels(skeleton):
-    pixels = 0
-    for i in range(0, len(skeleton)):
-        for j in range(0, len(skeleton[0])):
-            if skeleton[i][j] != 0:
-                pixels += 1
-    return pixels
+#------------------------------------------------------------------------------------
 
+
+#-----------------------------------MY CODE-----------------------------------
+
+#Takes a normalized version of the skeleton image (1 = white, 0 = black) and returns the Min and Max X and Y values of the worm in the image
 def findBoundingCoords(normalized_skeleton_image):
 	xMin=1000
 	yMin=1000
@@ -122,73 +129,121 @@ def findBoundingCoords(normalized_skeleton_image):
 		y+=1
 	return xMin,xMax,yMin,yMax
 
-def findRectCoords(image, endpoints, xMin, xMax, yMin, yMax):
+#Takes the worm and crops it into a rectangle that is created along the line drawn between the two endpoints of the worm
+def rectangleCrop(image, endpoints, xMin, xMax, yMin, yMax):
 	xDist = xMax-xMin
 	yDist = yMax-yMin
-	endpoint_line_slope = (endpoints[1][0]-endpoints[0][0])/(endpoints[1][1]-endpoints[0][1])
-	endpoint_line_y_intercept = endpoints[0][0] - (endpoint_line_slope*endpoints[0][1])
+	endpoint_x1 = endpoints[0][1]
+	endpoint_x2 = endpoints[1][1]
+	endpoint_y1 = endpoints[0][0]
+	endpoint_y2 = endpoints[1][0]
+
+	#Find equation of line between endpoints
+	endpoint_line_slope = (endpoint_y2-endpoint_y1)/(endpoint_x2-endpoint_x1)
+	endpoint_line_y_intercept = endpoint_y1 - (endpoint_line_slope*endpoint_x1)
+
+	#Make sure we dont divide by zero and calculate the slope of the line perpendicular to the line between endpoints to use as rectangle boundaries
 	if endpoint_line_slope==0:
 		perpendicular_slope=0
 	else:
 		perpendicular_slope = -(1 / endpoint_line_slope)
 
+	#Array we will put the coordinates of the rectangle corners in
 	corners = []
-	if (xDist>yDist):
-		bottomY = (endpoint_line_slope*xMin) + endpoint_line_y_intercept
-		topY = (endpoint_line_slope*xMax) + endpoint_line_y_intercept
 
-		perpendicular_top_y_intercept = topY - (perpendicular_slope*xMax)
-		perpendicular_bottom_y_intercept = bottomY - (perpendicular_slope*xMin)
+	#Special case for if the slope of the endpoint line is 0
+	if endpoint_line_slope==0:
+		#Use length of line extended to the min and max X of the worm to determine how thick rectangle will be
+		if xDist <= 100:
+			rect_thick = 4
+		elif xDist > 100 and xDist < 300:
+			rect_thick = 6
+		else:
+			rect_thick = 8
 
+		#Set the bounding x and y coordinates of the rectangle corners
+		top_negative_x = xMax
+		top_negative_y = xMax - rect_thick
+		top_positive_x = xMax
+		top_positive_y = xMax + rect_thick
+		bottom_negative_x = xMin
+		bottom_negative_y = xMin - rect_thick
+		bottom_positive_x = xMin
+		bottom_positive_y = xMin + rect_thick
+
+	#If the worm is horizontal, solve for rectangle like this:,
+	elif ((abs(endpoint_line_slope)<1) or (abs(endpoint_line_slope==1) and xDist>yDist)):
+		#Extending endpoint line to the xMin and xMax boundaries of the whole worm
+		y_for_x_min = xMin * endpoint_line_slope + endpoint_line_y_intercept
+		y_for_x_max = xMax * endpoint_line_slope + endpoint_line_y_intercept
+
+		#Find length of new line and use to determine how thick the rectangle will be
+		line_length = math.dist((xMin, y_for_x_min), (xMax, y_for_x_max))
+		if line_length <= 100:
+			rect_thick = 4
+		elif line_length > 100 and line_length < 300:
+			rect_thick = 6
+		else:
+			rect_thick = 8
+
+		#Find the equation of the two lines perpendicular to the endpoint line that intersect at the xMin and xMax
+		perpendicular_top_y_intercept = y_for_x_min - (perpendicular_slope*xMax)
+		perpendicular_bottom_y_intercept = y_for_x_max - (perpendicular_slope*xMin)
+
+		#Really ugly equation I derived to calculate the coordinates for each perpendicular line that extend it by rect_thick in each direction (the corners of the rectangle)
 		x = Symbol('x')
-		top_result = solve(((x - xMax) * (x - xMax)) + (((perpendicular_slope * x + perpendicular_top_y_intercept) - topY) * ((perpendicular_slope * x + perpendicular_top_y_intercept) - topY)) - (5 * 5), x)
-		bottom_result = solve(((x - xMin) * (x - xMin)) + (((perpendicular_slope * x + perpendicular_bottom_y_intercept) - bottomY) * ((perpendicular_slope * x + perpendicular_bottom_y_intercept) - bottomY)) - (5 * 5), x)
-		print(bottom_result)
+		top_result = solve(((x - xMax) * (x - xMax)) + (((perpendicular_slope * x + perpendicular_top_y_intercept) - y_for_x_max) * ((perpendicular_slope * x + perpendicular_top_y_intercept) - y_for_x_max)) - (rect_thick * rect_thick), x)
+		bottom_result = solve(((x - xMin) * (x - xMin)) + (((perpendicular_slope * x + perpendicular_bottom_y_intercept) - y_for_x_min) * ((perpendicular_slope * x + perpendicular_bottom_y_intercept) - y_for_x_min)) - (rect_thick * rect_thick), x)
+
+		#Get the x values of the 4 points
 		top_negative_x = top_result[0].evalf()
 		top_positive_x = top_result[1].evalf()
 		bottom_negative_x = bottom_result[0].evalf()
 		bottom_positive_x = bottom_result[1].evalf()
 
-
+		#Get the y values of the 4 points
 		top_negative_y = (top_negative_x * perpendicular_slope) + perpendicular_top_y_intercept
 		top_positive_y = (top_positive_x * perpendicular_slope) + perpendicular_top_y_intercept
 		bottom_negative_y = (bottom_negative_x * perpendicular_slope) + perpendicular_bottom_y_intercept
 		bottom_positive_y = (bottom_positive_x * perpendicular_slope) + perpendicular_bottom_y_intercept
 
-		'''
-		y_top_left_corner = (perpendicular_slope*(xMax-5)) + perpendicular_top_y_intercept
-		y_top_right_corner = (perpendicular_slope*(xMax+5)) + perpendicular_top_y_intercept
-		y_bottom_left_corner = (perpendicular_slope*(xMin-5)) + perpendicular_bottom_y_intercept
-		y_bottom_right_corner = (perpendicular_slope * (xMin + 5)) + perpendicular_bottom_y_intercept
-		'''
-
+	#If worm is vertical, solve for rectangle like this:
 	else:
+		#Extending endpoint line to the yMin and yMax boundaries of the whole worm
 		x1 = (yMin-endpoint_line_y_intercept)/endpoint_line_slope
 		x2 = (yMax-endpoint_line_y_intercept)/endpoint_line_slope
+
+		# Find length of new line and use to determine how thick the rectangle will be
+		line_length = math.dist((x1, yMin), (x2, yMax))
+		if line_length <= 100:
+			rect_thick = 4
+		elif line_length > 100 and line_length < 300:
+			rect_thick = 6
+		else:
+			rect_thick = 8
+
+		#Find the equation of the two lines perpendicular to the endpoint line that intersect at the yMin and yMax
 		perpendicular_top_y_intercept = yMax - (perpendicular_slope * x2)
 		perpendicular_bottom_y_intercept = yMin - (perpendicular_slope * x1)
 
+		# Really ugly equation I derived to calculate the coordinates for each perpendicular line that extend it by rect_thick in each direction (the corners of the rectangle)
 		x = Symbol('x')
-		bottom_result = solve(((x - x1) * (x - x1)) + (((perpendicular_slope * x + perpendicular_bottom_y_intercept) - yMin) * ((perpendicular_slope * x + perpendicular_bottom_y_intercept) - yMin)) - (5 * 5), x)
-		top_result = solve(((x - x2) * (x - x2)) + (((perpendicular_slope * x + perpendicular_top_y_intercept) - yMax) * ((perpendicular_slope * x + perpendicular_top_y_intercept) - yMax)) - (5 * 5), x)
-		print(bottom_result)
+		bottom_result = solve(((x - x1) * (x - x1)) + (((perpendicular_slope * x + perpendicular_bottom_y_intercept) - yMin) * ((perpendicular_slope * x + perpendicular_bottom_y_intercept) - yMin)) - (rect_thick * rect_thick), x)
+		top_result = solve(((x - x2) * (x - x2)) + (((perpendicular_slope * x + perpendicular_top_y_intercept) - yMax) * ((perpendicular_slope * x + perpendicular_top_y_intercept) - yMax)) - (rect_thick * rect_thick), x)
+
+		# Get the x values of the 4 points
 		top_negative_x = top_result[0].evalf()
 		top_positive_x = top_result[1].evalf()
 		bottom_negative_x = bottom_result[0].evalf()
 		bottom_positive_x = bottom_result[1].evalf()
 
+		# Get the y values of the 4 points
 		top_negative_y = (top_negative_x * perpendicular_slope) + perpendicular_top_y_intercept
 		top_positive_y = (top_positive_x * perpendicular_slope) + perpendicular_top_y_intercept
 		bottom_negative_y = (bottom_negative_x * perpendicular_slope) + perpendicular_bottom_y_intercept
 		bottom_positive_y = (bottom_positive_x * perpendicular_slope) + perpendicular_bottom_y_intercept
 
-		'''
-		y_top_left_corner = (perpendicular_slope * (x2 - 5)) + perpendicular_top_y_intercept
-		y_top_right_corner = (perpendicular_slope * (x2 + 5)) + perpendicular_top_y_intercept
-		y_bottom_left_corner = (perpendicular_slope * (x1 - 5)) + perpendicular_bottom_y_intercept
-		y_bottom_right_corner = (perpendicular_slope * (x1 + 5)) + perpendicular_bottom_y_intercept
-		'''
-
+	#Put the x and y values of the found rectangle corners into an array
 	corners = np.array([
 		[[int(top_negative_x), int(top_negative_y)]],
 		[[int(top_positive_x), int(top_positive_y)]],
@@ -196,35 +251,26 @@ def findRectCoords(image, endpoints, xMin, xMax, yMin, yMax):
 		[[int(bottom_positive_x), int(bottom_positive_y)]]
 	])
 
+	#Find the rectangle between the 4 corners
 	rect = cv2.minAreaRect(corners)
 	box = cv2.boxPoints(rect)
 	box = np.intp(box)
-	# print("bounding box: {}".format(box))
-	cv2.drawContours(input, [box], 0, (255, 0, 255), 1)
-	#width = int(rect[1][0])
-	#height = int(rect[1][1])
-	#src_pts = box.astype("float32")
-	#dst_pts = np.array([[0, height - 1], [0, 0], [width - 1, 0], [width - 1, height - 1]], dtype="float32")
-	#M = cv2.getPerspectiveTransform(src_pts, dst_pts)
-	#warped = cv2.warpPerspective(image, M, (width, height))
 
-	return image
+	#Find width and height of rectangle
+	width = int(rect[1][0])
+	height = int(rect[1][1])
+	src_pts = box.astype("float32")
 
+	#Find the matrix that warps/crops the image to the rectangle idk I stole this snippet from online and it works
+	dst_pts = np.array([[0, height - 1], [0, 0], [width - 1, 0], [width - 1, height - 1]], dtype="float32")
+	M = cv2.getPerspectiveTransform(src_pts, dst_pts)
+	warped = cv2.warpPerspective(image, M, (width, height))
 
-def giantDistanceAwayEquation(x1, y1, slope, y_intercept, dist):
-	x = Symbol('x')
-	result = solve(((x-x1)*(x-x1)) + (((slope*x + y_intercept)-y1)*((slope*x + y_intercept)-y1)) - (dist*dist), x)
-	negative_x2 = result[0].evalf()
-	positive_x2 = result[1].evalf()
+	#Return the rectangle image (it has to be warped bc the rectangles are mostly on a diagonal axis)
+	return warped
 
-	negative_y2 = (negative_x2 * slope) + y_intercept
-	positive_y2 = (positive_x2 * slope) + y_intercept
-	return negative_x2, negative_y2, positive_x2, positive_y2
-
-negative_x2, negative_y2, positive_x2, positive_y2 = giantDistanceAwayEquation(-5, 2, -2, -10,3)
-
-#print(negative_x2, negative_y2, positive_x2, positive_y2)
-
+#Testing code I was using for one image
+'''
 input = cv2.imread('./test/alive/alive 3.png')
 input = cv2.cvtColor(input, cv2.COLOR_BGR2GRAY)
 skeleton_image = zhangSuen(input).astype("float32")
@@ -238,29 +284,31 @@ if len(endpoints)==2:
 plt.figure()
 plt.imshow(result)
 plt.show()
-
 '''
+
+
+#Applying rectangle transformation to all the worms in the dataset
+i=101
+j=101
 data = []
-i=79
-j=1
-for file in os.listdir('./test/aliverest/'):
-	if i==24:
-		i+=1
-	image = './test/aliverest/' + file
+for file in os.listdir('./test/alive/'):
+	image = './test/alive/' + file
 	input = cv2.imread(image)
 	input = cv2.cvtColor(input, cv2.COLOR_BGR2GRAY)
 
+	#Find the skeleton of the worm
 	skeleton_image = zhangSuen(input).astype("float32")
+	#Normalize it
 	normalized_skeleton_image = skeleton_image / 255.0
-
+	#Find the endpoints of the worm
 	endpoints = findEndpoints(skeleton_image)
+	#If the worm has two endpoints (it is not a cluster a worm curled up in a circle) proceed
 	if len(endpoints)==2:
+		#Find the bounding coordinates of the worm
 		xMin,xMax,yMin,yMax = findBoundingCoords(normalized_skeleton_image)
-
-		warped = findRectCoords(input, endpoints, xMin, xMax, yMin, yMax)
-		#area_between = findAreaBetweenLineAndWorm(normalized_skeleton_image, line_image, xMin, xMax, yMin, yMax)
-
-		#data.append([area_between, 1])
+		#Get the rectangle iamge
+		warped = rectangleCrop(input, endpoints, xMin, xMax, yMin, yMax)
+		#Write it to a new directory
 		directory = './rectangles/alive/aliverect ' + str(i) + '.png'
 		cv2.imwrite(directory, warped)
 		print(i, ' alive picture added')
@@ -268,27 +316,20 @@ for file in os.listdir('./test/aliverest/'):
 	else:
 		continue
 
+#Do the same for the directory of dead worms
 for file in os.listdir('./test/dead/'):
 	image = './test/dead/' + file
 	input = cv2.imread(image)
 	input = cv2.cvtColor(input, cv2.COLOR_BGR2GRAY)
-
 	skeleton_image = zhangSuen(input).astype("float32")
 	normalized_skeleton_image = skeleton_image / 255.0
-
 	endpoints = findEndpoints(skeleton_image)
 	if len(endpoints)==2:
 		xMin,xMax,yMin,yMax = findBoundingCoords(normalized_skeleton_image)
-
-		warped = findRectCoords(input, endpoints, xMin, xMax, yMin, yMax)
-		# area_between = findAreaBetweenLineAndWorm(normalized_skeleton_image, line_image, xMin, xMax, yMin, yMax)
-
-		# data.append([area_between, 1])
+		warped = rectangleCrop(input, endpoints, xMin, xMax, yMin, yMax)
 		directory = './rectangles/dead/deadrect ' + str(j) + '.png'
 		cv2.imwrite(directory, warped)
-		print(i, ' dead picture added')
+		print(j, ' dead picture added')
 		j += 1
 	else:
 		continue
-
-'''
