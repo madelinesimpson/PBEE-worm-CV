a1ae2b5b1b73e128079ae6b3a689c3e76c9428aa
madelinesimpson <91549090+madelinesimpson@users.noreply.github.com>
2023-12-07 13:30:11 -0500

    Update cluster_run.py

--- cluster_run.py
+++ cluster_run.py
@@ -2,14 +2,12 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 import math
-
 from numpy import ndarray
-
 import individual_worm_data as worm
 import cluster_worm_data as cluster
 from random import randint
 
-image = cv2.imread('./cluster 2.png')
+image = cv2.imread('./cluster/cluster 11.png')
 
 contour_image = image.copy()
 contour_image = cv2.cvtColor(contour_image, cv2.COLOR_BGR2GRAY)
@@ -23,11 +21,17 @@ xMin, xMax, yMin, yMax = worm.findBoundingCoords(normalized)
 xMin, xMax, yMin, yMax = worm.padBounding(xMin, xMax, yMin, yMax)
 skeleton = worm.findSkeleton(skeleton, xMin, xMax, yMin, yMax)
 
+
+plt.figure()
+plt.imshow(skeleton)
+plt.show()
+
+
 #Getting intersections and endpoints
 endpoints = cluster.find_endpoints(skeleton)
 intersections, removed_endpoints, new_endpoints, new_skeleton = cluster.find_intersections(skeleton, endpoints)
 intersections = cluster.remove_repeat_intersections(intersections, 8, new_skeleton)
-
+print(new_endpoints)
 #Removing endpoints that have been marked to be removed (false endpoints)
 for point in endpoints:
     if point in removed_endpoints:
@@ -35,11 +39,56 @@ for point in endpoints:
 for point in new_endpoints:
     endpoints.append(point)
 
+'''
+new_skeleton = cv2.cvtColor(new_skeleton.astype(np.uint8), cv2.COLOR_GRAY2RGB)
+print(endpoints)
+print(intersections)
+for inter in intersections:
+    new_skeleton[inter[0]][inter[1]][0] = 255
+    new_skeleton[inter[0]][inter[1]][1] = 0
+    new_skeleton[inter[0]][inter[1]][2] = 0
+
+for end in endpoints:
+    new_skeleton[end[0]][end[1]][0] = 0
+    new_skeleton[end[0]][end[1]][1] = 0
+    new_skeleton[end[0]][end[1]][2] = 255
+
+plt.figure()
+plt.imshow(new_skeleton)
+plt.show()
+'''
+
 #Getting branches from endpoint to intersection and branches between intersections, then removing repeat branches
 branches, undone_points = cluster.get_branches(intersections, endpoints, new_skeleton)
 other_branches = cluster.get_paths_between_intersections(intersections, undone_points, new_skeleton)
 for branch in other_branches:
     branches.append(branch)
+
+new_skeleton = cv2.cvtColor(new_skeleton.astype(np.uint8), cv2.COLOR_GRAY2RGB)
+for branch in branches:
+    r = randint(100,255)
+    g = randint(100, 255)
+    b = randint(100, 255)
+    path = branch['path']
+    for point in path:
+        new_skeleton[point[0]][point[1]][0] = r
+        new_skeleton[point[0]][point[1]][1] = g
+        new_skeleton[point[0]][point[1]][2] = b
+
+for inter in intersections:
+    new_skeleton[inter[0]][inter[1]][0] = 255
+    new_skeleton[inter[0]][inter[1]][1] = 0
+    new_skeleton[inter[0]][inter[1]][2] = 0
+
+for end in endpoints:
+    new_skeleton[end[0]][end[1]][0] = 0
+    new_skeleton[end[0]][end[1]][1] = 0
+    new_skeleton[end[0]][end[1]][2] = 255
+
+plt.figure()
+plt.imshow(new_skeleton)
+plt.show()
+
 
 #unique_branches = cluster.remove_repeat_branches(branches)
 counter = 0
